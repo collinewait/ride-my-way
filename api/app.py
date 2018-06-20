351540109225e95d47129cfeaa2cb2b0e41d2822
Waitire Colline <collinewait17@gmail.com>
2018-06-20 12:52:43 +0300

    Add app.py as the Main root app of the api

---
+++ api/app.py
@@ -0,0 +1,24 @@
+"""
+Main root app of the api
+"""
+
+import sys
+import os
+
+from flask import Flask
+from flask_cors import CORS
+
+sys.path.append(os.path.pardir)
+
+from api.config import HOST, PORT, DEBUG, ENVIRONMENT, TESTING
+from api.urls import Urls
+
+APP = Flask(__name__)
+APP.testing = TESTING
+APP.env = ENVIRONMENT
+
+Urls.generateUrl(APP)
+
+CORS(APP)
+if __name__ == '__main__':
+    APP.run(debug=DEBUG, host=HOST, port=PORT)
