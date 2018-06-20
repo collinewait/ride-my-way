abac2bd181766c530a6e2f1599b132e79a8b92c0
Waitire Colline <collinewait17@gmail.com>
2018-06-20 13:50:27 +0300

    Add the test file to be used to test end points

---
+++ api/tests/test_ride.py
@@ -0,0 +1,10 @@
+"""
+This module contains tests for the api end points.
+"""
+from unittest import TestCase
+
+class TestRideTestCase(TestCase):
+    """
+    Tests run for the api end pints.
+    """
+    pass
