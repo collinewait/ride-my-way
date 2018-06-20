a4b18b196c46b2c980fcc95b0cb62b14ff29d929
Waitire Colline <collinewait17@gmail.com>
2018-06-20 14:57:56 +0300

    Add views file to handle url end points requests.

---
+++ api/views.py
@@ -0,0 +1,10 @@
+"""
+This module provides responses to url requests.
+"""
+from flask.views import MethodView
+
+class RideViews(MethodView):
+    """
+    This clas contains methods that respond to various url end points.
+    """
+    pass
