32db719a8d76bf119423715d7bbfb9b4ad336bbe
Waitire Colline <collinewait17@gmail.com>
2018-06-20 16:54:26 +0300

    [#158452273] Define test variables and initialize app

--- api/tests/test_ride.py
+++ api/tests/test_ride.py
@@ -2,9 +2,13 @@
 This module contains tests for the api end points.
 """
 from unittest import TestCase
+from api.app import APP
 
 class TestRideTestCase(TestCase):
     """
     Tests run for the api end pints.
     """
-    pass
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = APP
+        self.client = self.app.test_client
