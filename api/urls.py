fc975d6a374f2aa3b48e3ea3085f99e0c383ba7f
Waitire Colline <collinewait17@gmail.com>
2018-06-20 14:20:08 +0300

    Add Url s file to be used to specify the url end points

---
+++ api/urls.py
@@ -0,0 +1,16 @@
+"""
+This module handels requests to urls.
+"""
+
+class Urls(object):
+    """
+   Class to generate urls
+    """
+    @staticmethod
+    def generate_url(app):
+        """
+         Generates urls on the app context
+        :param: app: takes in the app variable
+        :return: urls
+        """
+        pass
