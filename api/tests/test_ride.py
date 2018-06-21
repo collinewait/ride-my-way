5b88acc4dd0de8d011a710bb50dfe1902ac6d1bb
Waitire Colline <collinewait17@gmail.com>
2018-06-21 12:29:46 +0300

    [#158452376] Enable API to return a single ride offer
    It returns status code of 200, ride dictionary is contained in the response
    The error_message attribute set to false
    It returns the exact number of items contained in the ride dictionary

--- api/tests/test_ride.py
+++ api/tests/test_ride.py
@@ -2,9 +2,25 @@
 This module contains tests for the api end points.
 """
 from unittest import TestCase
-
+from api.app import APP
 class TestRideTestCase(TestCase):
     """
     Tests run for the api end pints.
     """
-    pass
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = APP
+        self.client = self.app.test_client
+
+    def test_get_one_ride_offer(self):
+        """
+        Test an item is returned on a get request
+        :return:
+        """
+        response = self.client().get('/api/v1/rides/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("ride", response.json)
+        self.assertIn(False, response.json.values())
+        self.assertIn("error_message", response.json)
+        self.assertIsInstance(response.json['ride'], dict)
+        self.assertEqual(len(response.json['ride']), 7)
