5e4b99704c8c94553f710a4cc329ce650a90948c
Waitire Colline <collinewait17@gmail.com>
2018-06-21 23:29:05 +0300

    [#158452452] create a ride offer
    Enable creation of a ride offer om the api
    return status code of 201
    A ride is returned in the response
    Ride added successfully is returned in the response
    Added ride is in json format

--- api/tests/test_ride.py
+++ api/tests/test_ride.py
@@ -2,9 +2,33 @@
 This module contains tests for the api end points.
 """
 from unittest import TestCase
+from datetime import datetime
+from flask import json
+from api.app import APP
 
 class TestRideTestCase(TestCase):
     """
     Tests run for the api end pints.
     """
-    pass
+    def setUp(self):
+        self.app = APP
+        self.client = self.app.test_client
+
+    def test_post_creates_a_ride_offer(self):
+        """
+        This method tests for the creation of a ride offer
+        """
+        date_time = datetime.now()
+        depart_date = date_time.strftime("%x")
+        depart_time = date_time.strftime("%X")
+
+        response = self.client().post('/api/v1/rides/', data=json.dumps(
+            dict(driver_firstname="Jack", driver_lastname="Ma", destination="Mbarara",
+                 departure_date=depart_date, departure_time=depart_time,
+                 number_of_passengers=2)), content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertIn("ride", response.json)
+        self.assertIn("message", response.json)
+        self.assertIn("Ride added successfully", response.json['message'])
+        self.assertTrue(response.json['ride'])
--- api/views.py
+++ api/views.py
@@ -1,10 +1,29 @@
 """
 This module provides responses to url requests.
 """
+from flask import jsonify, request
 from flask.views import MethodView
 
 class RideViews(MethodView):
     """
     This clas contains methods that respond to various url end points.
     """
-    pass
+    rides = []
+    def post(self):
+        """"
+        Handles post requests
+        :return:
+        """
+
+        ride = {
+            "id": len(self.rides) + 1,
+            "driver_firstname": request.json['driver_firstname'],
+            "driver_lastname": request.json['driver_lastname'],
+            "destination": request.json['destination'],
+            "departure_date": request.json['departure_date'],
+            "departure_time": request.json['departure_time'],
+            "number_of_passengers": request.json['number_of_passengers'],
+        }
+        self.rides.append(ride)
+        return jsonify({"status_code": 201, "ride": ride,
+                        "message": "Ride added successfully"}), 201
