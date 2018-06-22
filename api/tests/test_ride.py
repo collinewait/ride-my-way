3d300d5f59b24623c8d29c215c27f5ec15a26786
Waitire Colline <collinewait17@gmail.com>
2018-06-22 04:41:00 +0300

    [#158452742] Enable joining a ride offer
    returns json response with status code of 201

--- api/tests/test_ride.py
+++ api/tests/test_ride.py
@@ -2,9 +2,28 @@
 This module contains tests for the api end points.
 """
 from unittest import TestCase
+from flask import json
+from api.app import APP
 
 class TestRideTestCase(TestCase):
     """
     Tests run for the api end pints.
     """
-    pass
+    def setUp(self):
+        self.app = APP
+        self.client = self.app.test_client
+
+    def test_post_joins_a_ride_offer(self):
+        """
+        This method tests the joinig of a ride offer
+        """
+
+        response = self.client().post('/api/v1/rides/1/requests', data=json.dumps(
+            dict(passenger_name="Jack", passenger_id=123, passenger_contact="0771462657"
+                )), content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertIn("request", response.json)
+        self.assertIn("message", response.json)
+        self.assertEqual("request sent successfully", response.json['message'])
+        self.assertTrue(response.json['request'])
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 """
 This module handels requests to urls.
 """
+from api.views import RideViews
 
 class Urls(object):
     """
@@ -13,4 +14,7 @@ class Urls(object):
         :param: app: takes in the app variable
         :return: urls
         """
-        pass
+
+        ride_view = RideViews.as_view('ride_api')
+        app.add_url_rule('/api/v1/rides/<int:ride_id>/requests',
+                         view_func=ride_view, methods=['POST',])
