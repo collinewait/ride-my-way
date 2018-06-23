f77505844d4f20d800534acac7716ee5af92a211
Waitire Colline <collinewait17@gmail.com>
2018-06-23 17:36:08 +0300

    Remove false from test succes messages on get requests

--- api/tests/test_ride.py
+++ api/tests/test_ride.py
@@ -26,7 +26,7 @@ class TestRideTestCase(TestCase):
         self.assertIn(1, response.json["rides"][0].values())
         self.assertIn(2, response.json["rides"][1].values())
         self.assertIn("message", response.json)
-        self.assertIn(False, response.json.values())
+        self.assertIn("results retrieved successfully", response.json.values())
 
     def test_get_one_ride_offer(self):
         """
@@ -36,7 +36,7 @@ class TestRideTestCase(TestCase):
         response = self.client().get('/api/v1/rides/1')
         self.assertEqual(response.status_code, 200)
         self.assertIn("ride", response.json)
-        self.assertIn(False, response.json.values())
+        self.assertIn("result retrieved successfully", response.json.values())
         self.assertIn("message", response.json)
         self.assertIsInstance(response.json['ride'], dict)
         self.assertEqual(len(response.json['ride']), 7)
